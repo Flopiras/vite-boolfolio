1654fe90840758ea8f0d3a7802c67fb27af92d6e
Flopiras <Flopiras@yahoo.it>
2023-09-11 16:50:08 +0200

    add component for projects' list

---
+++ src/components/projects/ProjectsList.vue
@@ -0,0 +1,17 @@
+<script>
+import ProjectCard from './ProjectCard.vue';
+
+export default {
+    components: { ProjectCard },
+    data: () => ({ projects: [] })
+};
+</script>
+
+<template>
+    <main>
+        <div class="container">
+            <!-- cards -->
+            <ProjectCard v-for="project in projects" :key="project.id" />
+        </div>
+    </main>
+</template>
