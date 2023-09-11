07371366ae0bd42c31392e72a680e545637f982d
Flopiras <Flopiras@yahoo.it>
2023-09-11 16:50:23 +0200

    add component for project' card

---
+++ src/components/projects/ProjectCard.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="card">
+        <!-- header -->
+        <div class="card-header"></div>
+        <!-- body -->
+        <div class="card-body">
+
+        </div>
+        <!-- footer -->
+        <div class="card-footer"></div>
+    </div>
+</template>
