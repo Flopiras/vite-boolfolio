b97f6d80087cd56158f2cf44d06a9e3e3f8b1185
Flopiras <Flopiras@yahoo.it>
2023-09-12 16:47:45 +0200

    put pagination in component

---
+++ src/components/AppPagination.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'AppPagination',
+    props: { links: Array }
+};
+</script>
+
+<template>
+    <div class="d-flex justify-content-center mb-4">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                <li class="page-item" :class="[{ active: link.active }, { disabled: !link.url }]"
+                    v-for="link in projects.links" :key="link.label">
+                    <button @click="fetchProjects(link.url)" class="page-link" v-html="link.label"></button>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
