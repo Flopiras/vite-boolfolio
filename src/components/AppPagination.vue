4953f63f110da05bb269477fedb75b07e444f37d
Flopiras <Flopiras@yahoo.it>
2023-09-12 16:58:50 +0200

    fix emit

--- src/components/AppPagination.vue
+++ src/components/AppPagination.vue
@@ -1,7 +1,8 @@
 <script>
 export default {
     name: 'AppPagination',
-    props: { links: Array }
+    props: { links: Array },
+    emit: ['change-page']
 };
 </script>
 
@@ -9,9 +10,9 @@ export default {
     <div class="d-flex justify-content-center mb-4">
         <nav aria-label="Page navigation example">
             <ul class="pagination">
-                <li class="page-item" :class="[{ active: link.active }, { disabled: !link.url }]"
-                    v-for="link in projects.links" :key="link.label">
-                    <button @click="fetchProjects(link.url)" class="page-link" v-html="link.label"></button>
+                <li class="page-item" :class="[{ active: link.active }, { disabled: !link.url }]" v-for="link in links"
+                    :key="link.label">
+                    <button @click="$emit('change-page', link.url)" class="page-link" v-html="link.label"></button>
                 </li>
             </ul>
         </nav>
