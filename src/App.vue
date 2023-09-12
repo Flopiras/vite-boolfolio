cfdf1061dba3788002466ec128c6631954eed7ba
Flopiras <Flopiras@yahoo.it>
2023-09-12 16:48:37 +0200

    add RouteView

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,8 @@
 <script>
-import axios from 'axios';
 import AppHeader from './components/AppHeader.vue';
-import ProjectsList from './components/projects/ProjectsList.vue';
 
 export default {
-
-  components: { AppHeader, ProjectsList },
-
-  data: () => ({
-    projects: {
-      data: [],
-      links: [],
-    }
-  }),
-  methods: {
-    fetchProjects(endpoint = 'http://127.0.0.1:8000/api/projects/') {
-      axios.get(endpoint)
-        .then(res => {
-          const { data, links } = res.data;
-          this.projects = { data, links };
-        })
-    },
-  },
-  created() {
-    this.fetchProjects();
-  }
+  components: { AppHeader },
 };
 </script>
 
@@ -32,20 +10,11 @@ export default {
   <!-- header -->
   <AppHeader />
 
-  <!-- main -->
-  <ProjectsList :projects="projects.data" />
-
-  <!-- pagination -->
-  <div class="d-flex justify-content-center mb-4">
-    <nav aria-label="Page navigation example">
-      <ul class="pagination">
-        <li class="page-item" :class="[{ active: link.active }, { disabled: !link.url }]" v-for="link in projects.links"
-          :key="link.label">
-          <button @click="fetchProjects(link.url)" class="page-link" v-html="link.label"></button>
-        </li>
-      </ul>
-    </nav>
-  </div>
+  <main>
+    <div class="container mt-4">
+      <RouteView />
+    </div>
+  </main>
 </template>
 
 <style></style>
