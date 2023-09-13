b76fd7c5dcec8db348b8e0706fe97d9a877a7c4b
Flopiras <Flopiras@yahoo.it>
2023-09-13 17:39:21 +0200

    add prevent and v-model on form

--- src/pages/ContactsPage.vue
+++ src/pages/ContactsPage.vue
@@ -1,27 +1,45 @@
+<script>
+import axios from 'axios';
+
+const emptyForm = { email: '', subject: '', content: '' }
+
+export default {
+    name: 'ContactsPage',
+    data: () => ({
+        form: emptyForm
+    }),
+    methods: {
+        sendForm() {
+
+        }
+    }
+}
+</script>
+
 <template>
     <header class="text-center">
         <h1>Contattaci</h1>
         <p class="mt-3 fs-5">Compila il form per mandare un messaggio all'autore dei progetti</p>
     </header>
     <!-- form -->
-    <form class="mt-5">
+    <form @submit.prevent="sendForm()" class="mt-5">
         <!-- email -->
         <div class="mb-3">
             <label for="email" class="form-label">Indirizzo email</label>
-            <input type="email" class="form-control" id="email" placeholder="name@example.com">
+            <input type="email" v-model.trim="form.email" class="form-control" id="email" placeholder="name@example.com">
             <small class="ms-2">A questo indirizzo riceverai la risposta</small>
         </div>
 
         <!-- email subject -->
         <div class="mb-3">
             <label for="subject" class="form-label">Oggetto della mail</label>
-            <input type="text" class="form-control" id="subject" placeholder="Oggetto">
+            <input type="text" v-model.trim="form.subject" class="form-control" id="subject" placeholder="Oggetto">
         </div>
 
         <!-- content -->
         <div class="mb-3">
             <label for="content" class="form-label">Contenuto della mail</label>
-            <textarea class="form-control" id="content" rows="5"></textarea>
+            <textarea class="form-control" v-model.trim="form.content" id="content" rows="5"></textarea>
         </div>
 
         <!-- button -->
